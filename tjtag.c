d72a2be7ce87157ed25d4f525fa7a0c5c76df479
Andwood <808692+andrea-wood@users.noreply.github.com>
2022-04-06 15:21:50 +0200

    Update tjtag.c
    test

--- tjtag.c
+++ tjtag.c
@@ -209,7 +209,7 @@ flash_area_type  flash_area_list[] =
     { size2MB,    "CFE",         0x1FC00000,  0x40000 },
     { size4MB,    "CFE",         0x1FC00000,  0x40000 },//256Kb
     { size8MB,    "CFE",         0x1C000000,  0x40000 },
-    { size16MB,   "CFE",         0x1F000000,  0x40000 }, //tornado - for alice
+    { size16MB,   "CFE",         0x1E000000,  0x020000  }, //tornado - for alice
     { size16MB,   "CFE_AGPF",    0x1E000000,  0x020000  }, //AGPF
 
     { size8MB,    "AR-CFE",         0xA8000000,  0x40000 },
@@ -233,7 +233,7 @@ flash_area_type  flash_area_list[] =
     { size2MB,    "KERNEL",      0x1FC40000,  0x1B0000 },
     { size4MB,    "KERNEL",      0x1FC40000,  0x3B0000 },//3776Kb
     { size8MB,    "KERNEL",      0x1C040000,  0x7A0000 },
-    { size16MB,   "KERNEL",      0x1C040000,  0x7A0000 },
+    { size16MB,   "KERNEL",      0x1E020000,  0x79FF6C },
     { size16MB,   "KERNEL_AGPF", 0x1E020000,  0x79FF6C },//AGPF
     { size8MB,    "AR-KERNEL",   0xA8040000,  0x7A0000 },
     { size16MB,   "AR-KERNEL",   0xA8040000,  0x7A0000 },
@@ -242,7 +242,7 @@ flash_area_type  flash_area_list[] =
     { size2MB,    "NVRAM",       0x1FDF0000,  0x10000 },
     { size4MB,    "NVRAM",       0x1FFF0000,  0x10000 },//64kb
     { size8MB,    "NVRAM",       0x1C7E0000,  0x20000 },
-    { size16MB,   "NVRAM",       0x1C7E0000,  0x20000 },
+    { size16MB,   "NVRAM",       0x1EFF0000,  0x010000 },
     { size16MB,   "NVRAM_AGPF",  0x1EFF0000,  0x010000  }, //AGPF
     { size16MB,   "PSI_NVRAM_AGPF",  0x1FFA0000,  0x01FF6C  }, //AGPF
     { size16MB,   "SCRATCH_PAD_AGPF",  0x1FFC0000, 0x01FF6C },//AGPF
@@ -260,7 +260,7 @@ flash_area_type  flash_area_list[] =
     { size4MB,    "WHOLEFLASH",  0x1FC00000,  0x400000 },//4Mb
     { size8MB,    "WHOLEFLASH",  0x1C000000,  0x800000 },
     //{ size16MB,   "WHOLEFLASH",  0x1C000000,  0x1000000 },
-    //{ size16MB,   "WHOLEFLASH",  0x1F000000,  0x1000000 },
+    { size16MB,   "WHOLEFLASH",  0x1E000000,             0x1000000 },
     { size16MB,   "WHOLEFLASH_AGPF", 0x1E000000, 0x1000000 },//AGPF
 
     { size8MB,    "AR-WHOLEFLASH",  0xA8000000,  0x800000 },
